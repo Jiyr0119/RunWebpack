d21feea532de663e964bbc8fe12cf6279760d5e8
jiyr <594402100@qq.com>
2018-01-25 09:22:16 +0800

    better man

--- app/Greeter.js
+++ app/Greeter.js
@@ -4,9 +4,14 @@ import styles from './Greeter.css'; //导入 .root 到 Greeter.js 中
 
 class Greeter extends Component {
     render() {
-        return (<div className={styles.root}> 
-	        		{ config.greetText }
-	        		<p className={styles.name}> { config.Name } 1314 </p> 
+        return (<div>
+	        		<div className={styles.root}> 
+		        		{ config.greetText }
+		        		<p className={styles.name}> { config.Name } 1314 </p> 
+		        	</div>
+		        	<div className={styles.my}>
+		        		{ config.wife }
+		        	</div>
 	        	</div>
         	);
         }
